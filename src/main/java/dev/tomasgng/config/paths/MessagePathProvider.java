670f793dfc9649e303d9d015bddd7564d8aac63b
TomasGnG <tomaskeder2005@gmail.com>
2024-10-03 23:31:36 +0200

    reformating and extending by ListPortals message

--- src/main/java/dev/tomasgng/config/paths/MessagePathProvider.java
+++ src/main/java/dev/tomasgng/config/paths/MessagePathProvider.java
@@ -12,11 +12,21 @@ public class MessagePathProvider {
     public static ConfigPair PORTAL_DESTINATION_CHANGED = new ConfigPair("portalDestinationChanged", "<green>You changed the portals destination.");
     public static ConfigPair SOURCE_LOCATION_BEING_USED = new ConfigPair("sourceLocationAlreadyBeingUsed", "<red>Portal cannot be created at this location.");
     public static ConfigPair PORTAL_DISTANCE_LIMIT_REACHED = new ConfigPair("maxPortalDistanceReached", "<red>The portal is too far away. The max portal distance is %maxportaldistance% blocks");
+
     public static ConfigPair COMMAND_NO_PERMISSIONS = new ConfigPair("command.noPermissions", "<red>You don't have permissions to execute this command.");
     public static ConfigPair COMMAND_USAGE = new ConfigPair("command.usage", List.of("<green>/utp reload <dark_gray>| <gray>Reload the config files",
                                                                                      "<green>/utp update <dark_gray>| <gray>Download the newest update"));
+
     public static ConfigPair COMMAND_RELOAD_SUCCESS = new ConfigPair("command.reloadSuccess", "<green>The config files have been reloaded successfully.");
+
     public static ConfigPair COMMAND_UPDATE_NO_UPDATE_AVAILABLE = new ConfigPair("command.updateNoUpdateAvailable", "<green>You are using the newest version.");
     public static ConfigPair COMMAND_UPDATE_SUCCESS = new ConfigPair("command.updateSuccess", "<green>The update have been downloaded. Restart the server to use the new version.");
     public static ConfigPair COMMAND_UPDATE_FAILURE = new ConfigPair("command.updateFailure", "<red>The download of the update has failed.");
+
+    public static ConfigPair COMMAND_LISTPORTALS_HEADMSG = new ConfigPair("command.listPortals.headMsg", List.of("<b><green>Your Portals",
+                                                                                                                 "<gray>Portal Limit: <yellow>%portallimit%",
+                                                                                                                 "<gray>Max Portal Distance: <yellow>%maxportaldistance%",
+                                                                                                                 "",
+                                                                                                                 "<red>Leftclick to remove a portal"));
+    public static ConfigPair COMMAND_LISTPORTALS_FORMAT = new ConfigPair("command.listPortals.", "<gray>%number% <dark_gray>| <yellow>%world%<dark_gray>[<yellow>%x%<dark_gray>, <yellow>%y%<dark_gray>, <yellow>%z%<dark_gray>]");
 }
